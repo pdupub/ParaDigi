866c65e1104b5d641bf6715ecce5ac8ac8e6c917
Peng Liu <pdupub@gmail.com>
2025-02-16 16:57:17 +0800

    refactor

---
+++ ParaDigi/ViewModels/SignUpViewModel.swift
@@ -0,0 +1,119 @@
+//
+//  SignUpViewModel.swift
+//  ParaDigi
+//
+//  Created by Peng Liu on 2025/2/16.
+//
+
+import SwiftUI
+
+
+class SignUpViewModel: ObservableObject {
+    @Published var randomImage: UIImage?
+    @Published var base64ImageString: String?
+    @Published var privateKey: String?
+    @Published var address: String?
+    @Published var keyValuePairs: [(key: String, value: String)] = [("nickname", ""),("", "")]
+    @Published var errorMessages: [String] = []
+    
+    @Published var isPrivateKeyVisible = false
+    @Published var isCopiedAddress = false
+    @Published var isCopiedPrivateKey = false
+
+    // 生成随机头像
+    func generateRandomImage() {
+        randomImage = ImageUtilities.generateRandomAvatarImage()
+        base64ImageString = ImageUtilities.imageToBase64(image: randomImage!)
+    }
+    
+    // 使用 CompatibleCrypto.swift 生成私钥和地址
+    func generatePrivateKeyAndAddress() {
+        let privateKeyData = CompatibleCrypto.generatePrivateKey()
+        privateKey = privateKeyData.map { String(format: "%02x", $0) }.joined()
+
+        let publicKeyData = CompatibleCrypto.generatePublicKey(privateKey: privateKeyData)
+        address = CompatibleCrypto.generateAddress(publicKey: publicKeyData)
+    }
+
+    // 拷贝地址到剪贴板
+    func copyAddressToClipboard() {
+        if let address = address {
+            UIPasteboard.general.string = address
+            isCopiedAddress = true
+        }
+    }
+
+    // 拷贝私钥到剪贴板
+    func copyPrivateKeyToClipboard() {
+        if let privateKey = privateKey {
+            UIPasteboard.general.string = privateKey
+            isCopiedPrivateKey = true
+        }
+    }
+
+    // 添加新的键值对输入框
+    func addKeyValuePair() {
+        keyValuePairs.append(("", ""))
+    }
+
+    // 创建用户的业务逻辑
+    func createUser() {
+        errorMessages.removeAll()  // 清空之前的错误信息
+
+        for index in (0..<keyValuePairs.count).reversed() {  // 使用 reversed 遍历，以防删除时出错
+            let (key, value) = keyValuePairs[index]
+            
+            // 如果 key 和 value 都为空，删除该行
+            if key.isEmpty && value.isEmpty {
+                keyValuePairs.remove(at: index)
+            } else {
+                // 如果只填写了 key 或 value，提示用户填写另一框
+                if key.isEmpty || value.isEmpty {
+                    let missingField = key.isEmpty ? "key" : "value"
+                    errorMessages.append("Please complete the \(missingField) (Line \(index + 1))")
+                }
+            }
+        }
+        errorMessages.reverse()
+
+        if errorMessages.isEmpty {
+            var contents:[QContent] = [
+                QContent(data: AnyCodable("avatar"), format: "key"), // Key
+                QContent(data: AnyCodable(base64ImageString!), format: "base64") // Value
+            ]
+            for index in (0..<keyValuePairs.count) {
+                let (key, value) = keyValuePairs[index]
+                contents.append(QContent(data: AnyCodable(key), format: "key")) // Key
+                contents.append(QContent(data: AnyCodable(value), format: "str")) // Value
+            }
+            
+            let last = ""
+            let nonce = 1
+            let references: [String] = []
+            let type: Int = 1
+
+            // 创建 UnsignedQuantum 对象
+            let unsignedQuantum = UnsignedQuantum(contents: contents, last: last, nonce: nonce, references: references, type: type)
+            if let jsonData = try? JSONEncoder().encode(unsignedQuantum) {
+                if let jsonString = String(data: jsonData, encoding: .utf8) {
+
+                    guard let privateKeyData = CompatibleCrypto.generatePrivateKey(fromString: privateKey!) else { return }
+                    let signatureData = CompatibleCrypto.signMessage(privateKey: privateKeyData, message: jsonData)
+                    let signature = signatureData.map { String(format: "%02x", $0) }.joined()
+                    let signedQuantum = SignedQuantum(unsignedQuantum: unsignedQuantum, signature: signature, signer: address)
+                    
+                    if let signedData = try? JSONEncoder().encode(signedQuantum) {
+                        if let signedString = String(data:signedData, encoding: .utf8) {
+                            print("Encoded JSON: \(signedString)")
+                        }
+                    }
+
+                    let publicKeyData = CompatibleCrypto.generatePublicKey(privateKey: privateKeyData)
+                    
+                    let isVerify = CompatibleCrypto.verifySignature(message: jsonData, signature: signatureData, publicKey: publicKeyData)
+                    print("Verify Signature: \(isVerify)")
+                }
+            }
+        }
+    }
+}
--- ParaDigi/Views/SignUpView.swift
+++ ParaDigi/Views/SignUpView.swift
@@ -7,24 +7,14 @@
 
 import SwiftUI
 
-
 struct SignUpView: View {
-    @State private var randomImage: UIImage?
-    @State private var base64ImageString: String?
-    @State private var privateKey: String?
-    @State private var address: String?
-    @State private var isPrivateKeyVisible = false
-    @State private var nickname: String = ""
-    @State private var keyValuePairs: [(key: String, value: String)] = [("nickname",""),("", "")]
-    
-    @State private var isCopiedAddress = false
-    @State private var isCopiedPrivateKey = false
-    @State private var errorMessages: [String] = []
+    @StateObject private var viewModel = SignUpViewModel()
+
     var body: some View {
         VStack {
             // 头像区域
             HStack {
-                if let image = randomImage {
+                if let image = viewModel.randomImage {
                     Image(uiImage: image)
                         .resizable()
                         .scaledToFit()
@@ -37,7 +27,7 @@ struct SignUpView: View {
                 }
                 
                 // 右侧按钮，重新生成头像
-                Button(action: generateRandomImage) {
+                Button(action: viewModel.generateRandomImage) {
                     Image(systemName: "arrow.clockwise.circle.fill")
                         .font(.title)
                         .foregroundColor(.blue)
@@ -47,13 +37,13 @@ struct SignUpView: View {
 
             // 显示地址
             HStack {
-                Text("Address: \(address ?? "building...")")
+                Text("Address: \(viewModel.address ?? "building...")")
                     .lineLimit(1)
                     .truncationMode(.middle)
                 
                 // 拷贝地址按钮
-                Button(action: copyAddressToClipboard) {
-                    Image(systemName: isCopiedAddress ? "checkmark.circle.fill" : "doc.on.clipboard.fill")
+                Button(action: viewModel.copyAddressToClipboard) {
+                    Image(systemName: viewModel.isCopiedAddress ? "checkmark.circle.fill" : "doc.on.clipboard.fill")
                         .foregroundColor(.blue)
                         .padding(.leading)
                 }
@@ -62,44 +52,41 @@ struct SignUpView: View {
 
             // 显示私钥
             HStack {
-                Text("Private Key: \(isPrivateKeyVisible ? privateKey ?? "building..." : String(repeating: "*", count: privateKey?.count ?? 0))")
+                Text("Private Key: \(viewModel.isPrivateKeyVisible ? viewModel.privateKey ?? "building..." : String(repeating: "*", count: viewModel.privateKey?.count ?? 0))")
                     .lineLimit(1)
                     .truncationMode(.middle)
                 
                 // 眼睛图标，切换私钥的可见性
-                Button(action: { isPrivateKeyVisible.toggle() }) {
-                    Image(systemName: isPrivateKeyVisible ? "eye.fill" : "eye.slash.fill")
+                Button(action: { viewModel.isPrivateKeyVisible.toggle() }) {
+                    Image(systemName: viewModel.isPrivateKeyVisible ? "eye.fill" : "eye.slash.fill")
                         .foregroundColor(.blue)
                         .padding(.leading)
                 }
                 
                 // 拷贝私钥按钮
-                Button(action: copyPrivateKeyToClipboard) {
-                    Image(systemName: isCopiedPrivateKey ? "checkmark.circle.fill" : "doc.on.clipboard.fill")
+                Button(action: viewModel.copyPrivateKeyToClipboard) {
+                    Image(systemName: viewModel.isCopiedPrivateKey ? "checkmark.circle.fill" : "doc.on.clipboard.fill")
                         .foregroundColor(.blue)
                         .padding(.leading)
                 }
             }
             .padding()
             
-            
-   
             // 动态增加键值对输入框
             VStack {
-                ForEach(0..<keyValuePairs.count, id: \.self) { index in
+                ForEach(0..<viewModel.keyValuePairs.count, id: \.self) { index in
                     HStack {
-                        TextField("Key", text: $keyValuePairs[index].key)
+                        TextField("Key", text: $viewModel.keyValuePairs[index].key)
                             .textFieldStyle(RoundedBorderTextFieldStyle())
                         
-                        TextField("Value", text: $keyValuePairs[index].value)
+                        TextField("Value", text: $viewModel.keyValuePairs[index].value)
                             .textFieldStyle(RoundedBorderTextFieldStyle())
                     }
-//                    .padding()
                 }
             }
             
             // 加号按钮，添加新的键值对输入框
-            Button(action: addKeyValuePair) {
+            Button(action: viewModel.addKeyValuePair) {
                 Image(systemName: "plus.circle.fill")
                     .font(.title)
                     .foregroundColor(.blue)
@@ -108,14 +95,14 @@ struct SignUpView: View {
             
             Spacer()
             // 显示错误信息
-            ForEach(errorMessages, id: \.self) { message in
+            ForEach(viewModel.errorMessages, id: \.self) { message in
                 Text(message)
                     .foregroundColor(.red)
                     .padding(.top, 5)
             }
             // 创建用户按钮
             Button("Create User") {
-                createUser()
+                viewModel.createUser()
             }
             .padding()
             .background(Color.blue)
@@ -123,110 +110,9 @@ struct SignUpView: View {
             .cornerRadius(10)
         }
         .onAppear {
-            generateRandomImage()
-            generatePrivateKeyAndAddress()
+            viewModel.generateRandomImage()
+            viewModel.generatePrivateKeyAndAddress()
         }
         .padding()
     }
-    
-    // 生成随机头像
-    private func generateRandomImage() {
-        randomImage = ImageUtilities.generateRandomAvatarImage()
-        base64ImageString = ImageUtilities.imageToBase64(image: randomImage!)
-    }
-    
-    // 使用 CompatibleCrypto.swift 生成私钥和地址
-    private func generatePrivateKeyAndAddress() {
-        let privateKeyData = CompatibleCrypto.generatePrivateKey()
-        privateKey = privateKeyData.map { String(format: "%02x", $0) }.joined()
-        
-        let publicKeyData = CompatibleCrypto.generatePublicKey(privateKey: privateKeyData)
-//        var publicKey = publicKeyData.map { String(format: "%02x", $0) }.joined()
-
-        address = CompatibleCrypto.generateAddress(publicKey: publicKeyData)
-    }
-
-    // 拷贝地址到剪贴板
-    private func copyAddressToClipboard() {
-        if let address = address {
-            UIPasteboard.general.string = address
-            isCopiedAddress = true
-        }
-    }
-    
-    // 拷贝私钥到剪贴板
-    private func copyPrivateKeyToClipboard() {
-        if let privateKey = privateKey {
-            UIPasteboard.general.string = privateKey
-            isCopiedPrivateKey = true
-        }
-    }
-    
-    // 添加新的键值对输入框
-    private func addKeyValuePair() {
-        keyValuePairs.append(("", ""))
-    }
-    
-    // 创建用户的业务逻辑
-    private func createUser() {
-        errorMessages.removeAll()  // 清空之前的错误信息
-
-        for index in (0..<keyValuePairs.count).reversed() {  // 使用 reversed 遍历，以防删除时出错
-            let (key, value) = keyValuePairs[index]
-            
-            // 如果 key 和 value 都为空，删除该行
-            if key.isEmpty && value.isEmpty {
-                keyValuePairs.remove(at: index)
-            } else {
-                // 如果只填写了 key 或 value，提示用户填写另一框
-                if key.isEmpty || value.isEmpty {
-                    let missingField = key.isEmpty ? "key" : "value"
-                    errorMessages.append("Please complete the \(missingField) (Line \(index + 1))")
-                }
-            }
-        }
-        errorMessages.reverse()
-       
-        if errorMessages.isEmpty {
-            var contents:[QContent] = [
-                QContent(data: AnyCodable("avatar"), format: "key"), // Key
-                QContent(data: AnyCodable(base64ImageString!), format: "base64") // Value
-            ]
-            for index in (0..<keyValuePairs.count) {
-                let (key, value) = keyValuePairs[index]
-                contents.append(QContent(data: AnyCodable(key), format: "key")) // Key
-                contents.append(QContent(data: AnyCodable(value), format: "str")) // Key
-            }
-            
-            let last = ""
-            let nonce = 1
-            let references: [String] = []
-            let type: Int = 1
-
-            // 创建 UnsignedQuantum 对象
-            let unsignedQuantum = UnsignedQuantum(contents: contents, last: last, nonce: nonce, references: references, type: type)
-            if let jsonData = try? JSONEncoder().encode(unsignedQuantum) {
-                if let jsonString = String(data: jsonData, encoding: .utf8) {
-
-                    guard let privateKeyData = CompatibleCrypto.generatePrivateKey(fromString: privateKey!) else { return }
-                    let signatureData = CompatibleCrypto.signMessage(privateKey: privateKeyData, message: jsonData)
-                    let signature = signatureData.map { String(format: "%02x", $0) }.joined()
-                    let signedQuantum = SignedQuantum(unsignedQuantum: unsignedQuantum, signature: signature, signer: address)
-                    
-                    if let signedData = try? JSONEncoder().encode(signedQuantum) {
-                        if let signedString = String(data:signedData, encoding: .utf8) {
-                            print("Encoded JSON: \(signedString)")
-                        }
-                    }
-
-                    let publicKeyData = CompatibleCrypto.generatePublicKey(privateKey: privateKeyData)
-                    
-                    let isVerify = CompatibleCrypto.verifySignature(message: jsonData, signature: signatureData, publicKey: publicKeyData)
-                    print("Verify Signature: \(isVerify)")
-                }
-            }
-        }
-
-    }
 }
-
