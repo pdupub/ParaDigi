236d42f5833778368f4ede81c5ff8c2d413a479f
Peng Liu <pdupub@gmail.com>
2025-03-23 10:49:01 +0800

    remove search bar when show feeddetail

--- ParaDigi/Views/SearchView.swift
+++ ParaDigi/Views/SearchView.swift
@@ -13,50 +13,52 @@ struct SearchView: View {
 
     var body: some View {
         VStack {
-            // 顶部的单行输入框
-            TextField("Search...", text: $viewModel.searchText, onCommit: {
-                viewModel.search(viewModel.searchText)
-            })
-            .textFieldStyle(RoundedBorderTextFieldStyle()) // 使用圆角样式
-            .padding()
-            .submitLabel(.search) // 将键盘上的“Return”按钮改为“Search”
-            .onAppear{
-                viewModel.setModelContext(modelContext: modelContext)
-            }
-            
+
             NavigationStack(path: $navigationPath) {
-                List{
-                    ForEach(viewModel.qs) { quantum in
-                        if let user = viewModel.fetchUserInfo(for: quantum.signer!, modelContext: modelContext) {
-                            ZStack {
-                                NavigationLink(value: quantum, label: {
-                                    EmptyView() // 隐藏默认的尖括号
-                                })
-                                .opacity(0) // 确保 NavigationLink 不显示任何视觉元素
-                                
-                                // 自定义点击区域
-                                Button(action: {
-                                    navigationPath.append(quantum) // 手动推送导航
-                                }) {
-                                    VStack(alignment: .leading) {
-                                        QuantumRowView(quantum: quantum, userInfo: user)
+                VStack{
+                    if navigationPath.isEmpty {
+                        
+                            // 顶部的单行输入框
+                            TextField("Search...", text: $viewModel.searchText, onCommit: {
+                                viewModel.search(viewModel.searchText)
+                            })
+                            .textFieldStyle(RoundedBorderTextFieldStyle()) // 使用圆角样式
+                            .padding()
+                            .submitLabel(.search) // 将键盘上的“Return”按钮改为“Search”
+                    }
+                    List{
+                        ForEach(viewModel.qs) { quantum in
+                            if let user = viewModel.fetchUserInfo(for: quantum.signer!, modelContext: modelContext) {
+                                ZStack {
+                                    NavigationLink(value: quantum, label: {
+                                        EmptyView() // 隐藏默认的尖括号
+                                    })
+                                    .opacity(0) // 确保 NavigationLink 不显示任何视觉元素
+                                    
+                                    // 自定义点击区域
+                                    Button(action: {
+                                        navigationPath.append(quantum) // 手动推送导航
+                                    }) {
+                                        VStack(alignment: .leading) {
+                                            QuantumRowView(quantum: quantum, userInfo: user)
+                                        }
                                     }
+                                    .buttonStyle(PlainButtonStyle()) // 移除按钮的默认样式
                                 }
-                                .buttonStyle(PlainButtonStyle()) // 移除按钮的默认样式
+                                .listRowInsets(EdgeInsets())
+                                .listRowSeparatorTint(.gray)
+                                
                             }
-                            .listRowInsets(EdgeInsets())
-                            .listRowSeparatorTint(.gray)
-                            
                         }
                     }
-                }
-                .listStyle(PlainListStyle())
-                .navigationDestination(for: SignedQuantum.self) { quantum in
-                    if let user = viewModel.fetchUserInfo(for: quantum.signer!, modelContext: modelContext) {
-                        FeedDetailView(quantum: quantum, userInfo: user)
-                            .onDisappear {
-//                                viewModel.refreshData()
-                            }
+                    .listStyle(PlainListStyle())
+                    .navigationDestination(for: SignedQuantum.self) { quantum in
+                        if let user = viewModel.fetchUserInfo(for: quantum.signer!, modelContext: modelContext) {
+                            FeedDetailView(quantum: quantum, userInfo: user)
+                                .onDisappear {
+                                    //                                viewModel.refreshData()
+                                }
+                        }
                     }
                 }
             }
